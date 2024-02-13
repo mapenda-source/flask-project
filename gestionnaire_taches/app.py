9737ccce210287d6c3e526b7d59fe6140664e7cc
Mapenda <mapendadioum3@gmail.com>
2024-02-12 22:21:45 -0500

    création de modèle

--- gestionnaire_taches/app.py
+++ gestionnaire_taches/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'  # Utilisez SQLite comme base de données
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    deadline = db.Column(db.Date)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
